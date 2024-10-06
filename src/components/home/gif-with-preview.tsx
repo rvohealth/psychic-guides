0ff59bbd3dcce5a5639b2b9b11e3fc79c1faede0
avocadojesus <jgarbey@gmail.com>
2024-10-06 13:55:23 -0700

    improve route, encryption, and cookie documentation
    also, add full-screen mode for gif banners

--- src/components/home/gif-with-preview.tsx
+++ src/components/home/gif-with-preview.tsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
+import { BsFullscreen, BsFullscreenExit } from 'react-icons/bs'
+import { IoIosPlay } from 'react-icons/io'
+import { IoStopSharp } from 'react-icons/io5'
 
 export default function GifWithPreview({ gifUrl, gifPreviewUrl }) {
   const [playing, setPlaying] = useState(false)
+  const [fullScreen, setFullScreen] = useState(false)
+
   return (
-    <div className={`gif-with-preview  ${playing ? 'playing' : 'stopped'}`}>
+    <div
+      className={`gif-with-preview  ${playing ? 'playing' : 'stopped'} ${fullScreen ? 'full-screen' : ''}`}
+    >
       <div
         className="banner-image"
         style={{ backgroundImage: `url(${playing ? gifUrl : gifPreviewUrl})` }}
       ></div>
-      <button onClick={() => setPlaying(!playing)}>
-        {playing ? 'STOP' : 'PLAY'}
+      <button className="pause-play-btn" onClick={() => setPlaying(!playing)}>
+        {playing ? <IoStopSharp /> : <IoIosPlay />}
+      </button>
+      <button
+        className="full-screen-btn"
+        onClick={() => setFullScreen(!fullScreen)}
+      >
+        {fullScreen ? <BsFullscreen /> : <BsFullscreenExit />}
       </button>
     </div>
   )
