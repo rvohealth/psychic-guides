d3b78f3b9900036435243866a3d58921131596c4
avocadojesus <fgarbutt@rvohealth.com>
2025-03-10 12:50:00 -0700

    add i18n docs, fix right sidebar item (#43)
    * RightSidebarItem -> RoutesOutput
    
    * add i18n docs
    
    * fix association decorator examples
    
    * new Deco pattern, new import pattern
    
    * @rvohealth -> @rvoh
    
    * add dream/psychic contributing docs
    
    * fix missing links

--- src/components/ui/right-sidebar-item.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react"
-
-export default function RightSidebarItem({
-  children,
-}: {
-  children: ReactNode
-}) {
-  return <div className="right-sidebar-item">{children}</div>
-}
